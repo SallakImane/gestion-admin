f4982ddacf5bdf456bddf1561f333404e02f1c5b
Sallak Imane <sallakimane9@gmail.com>
2019-09-30 17:30:58 +0100

    Add form register html

--- api-gestion/src/main/java/com/admin/apigestion/entities/User.java
+++ api-gestion/src/main/java/com/admin/apigestion/entities/User.java
@@ -15,6 +15,12 @@ import javax.persistence.Entity;
 @Builder
 @Entity
 public class User extends AbstractEntity {
+    @Column(name="first_name" ,nullable = false)
+    private String firstName;
+
+    @Column(name="last_name" ,nullable = false)
+    private String lastName;
+
     @Column(unique = true, nullable = false)
     private String email;
 

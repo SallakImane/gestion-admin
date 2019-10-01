b680520e4adfeaf9590eaff5becc574a01de6f7a
Sallak Imane <sallakimane9@gmail.com>
2019-10-01 08:41:08 +0100

    Finish form Register with Angular

--- angular-gestion/src/app/authentification/register/register.component.ts
+++ angular-gestion/src/app/authentification/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {MustMatch} from "./must-match.validator";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName :['',Validators.required],
+      lastName :['',Validators.required],
+      email :['',[Validators.required,Validators.email]],
+      phone :['',[Validators.required,Validators.minLength(6)]],
+      password :['',Validators.required],
+      confirmPassword :['',Validators.required],
+    },{
+      validators :MustMatch('password','confirmPassword')
+    });
   }
 
+  register() {
+    console.log(JSON.stringify(this.registerForm.value))
+  }
 }

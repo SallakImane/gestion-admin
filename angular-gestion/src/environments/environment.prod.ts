e835744a5262b1a37c854983772bd1bf3e60dd47
Sallak Imane <sallakimane9@gmail.com>
2019-10-28 16:26:08 +0100

    change apiurl in environment.prod.ts

--- angular-gestion/src/environments/environment.prod.ts
+++ angular-gestion/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8081/gestion-admin/api'
+  //apiUrl: 'http://localhost:8081/gestion-admin/api'
+  apiUrl: 'http://192.168.100.20:8080/gestion-admin/api'
 };
